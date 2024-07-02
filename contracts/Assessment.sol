a6f2a8110b17a0eb619aff902b040ccd5db71f99
prithvin06 <113163082+prithvin06@users.noreply.github.com>
2024-07-02 12:42:09 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    
+    string[] public array;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    constructor() payable {
+        array = ["Cat", "Mouse", "Rat", "Dog"];
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function getArray() public view returns(string[] memory) {
+        return array;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function addString(string memory _word) public payable {
+        array.push(_word);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+    function removeString(string memory _word) public {
+        for (uint i = 0; i < array.length; i++) {
+            if (keccak256(bytes(array[i])) == keccak256(bytes(_word))) {
+                for (uint j = i; j < array.length - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+                array.pop();
+                break;
+            }
         }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
     }
 }
